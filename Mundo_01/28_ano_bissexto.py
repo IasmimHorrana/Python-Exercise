08929181fe1537a2c03d0da8f2107ca5dc894a9c
Iasmim Horrana <horranaiasmim@gmail.com>
2024-07-05 22:12:41 -0300

    update

---
+++ Mundo_01/28_ano_bissexto.py
@@ -0,0 +1,17 @@
+#Exercise 28
+
+#Descrição: Saber se o ano é bissexto.
+
+from datetime import date
+
+ano = int(input("Que ano você deseja analisar? Coloque 0 para analisar o ano atual: "))
+
+if ano == 0:
+    ano = date.today().year
+
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print(f"O ano {ano} é bissexto!")
+else:
+    print(f"Opa, o ano {ano} não é bissexto.")
+
+
