20bbd5c81639f2988a6023f0d0ae605c97c520aa
Iasmim Horrana <horranaiasmim@gmail.com>
2024-07-12 20:19:44 -0300

    update

---
+++ Mundo_02/23_estrutura_repeticao.py
@@ -0,0 +1,15 @@
+#Exercise 23
+
+#Descrição: Estrutura de repetição.
+
+#For (mais usado quando você sabe o limite)
+for c in range(1, 10):
+    print(c)
+print("Fim.")
+
+#While (mais usado quando você não sabe o limite)
+c = 1
+while c < 10:
+    print(c)
+    c = c + 1
+print("Fim.")
